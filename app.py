573eabdce4a28ef5f4b5e1005aebf1ba04c1a320
WilisArum02 <148854173+WilisArum02@users.noreply.github.com>
2024-04-05 04:35:48 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+from views import *
+# from auth import *
+# from menu import *
+# from antrian import *
+# from pembayaran import *
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=False)
